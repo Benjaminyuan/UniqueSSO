01b84326b6253f97bdd22d7b067d90e369c03f61
xylonx <xylonx@dingtalk.com>
2021-09-26 11:22:26 +0800

    :wrench: update config

--- SSO/conf/conf.go
+++ SSO/conf/conf.go
@@ -1,29 +1,29 @@
 package conf
 
 import (
-	"context"
 	"net/url"
 	"regexp"
 	"sync"
-	"time"
 
-	"github.com/go-redis/redis/v8"
-	"github.com/sirupsen/logrus"
+	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/xylonx/zapx"
+	"go.uber.org/zap"
 )
 
 type Conf struct {
-	Application ApplicationConf `mapstructure:"application"`
-	Database    DatabaseConf    `mapstructure:"database"`
-	Redis       RedisConf       `mapstructure:"redis"`
-	Sms         SmsConf         `mapstructure:"sms"`
-	WorkWx      WorkWxConf      `mapstructure:"work_wx"`
+	Application  ApplicationConf  `mapstructure:"application"`
+	Database     DatabaseConf     `mapstructure:"database"`
+	Redis        RedisConf        `mapstructure:"redis"`
+	Sms          []SMSOptions     `mapstructure:"sms"`
+	WorkWx       WorkWxConf       `mapstructure:"work_wx"`
+	OpenPlatform OpenPlatformConf `mapstructure:"openplat_form"`
+	APM          APMConf          `mapstructure:"apm"`
 }
 type ApplicationConf struct {
 	Host            string           `mapstructure:"host"`
 	Port            string           `mapstructure:"port"`
+	Name            string           `mapstructure:"name"`
 	Mode            string           `mapstructure:"mode"`
 	ReadTimeout     int              `mapstructure:"read_timeout"`
 	WriteTimeout    int              `mapstructure:"write_timeout"`
@@ -41,10 +41,10 @@ type RedisConf struct {
 	DB       int    `mapstructure:"db"`
 }
 
-type SmsConf struct {
-	SecretId  string `mapstructure:"secret_id"`
-	SecretKey string `mapstructure:"secret_key"`
-	AppId     string `mapstructure:"app_id"`
+type SMSOptions struct {
+	Name       string `mapstructure:"name" validator:"oneof='verificationCode'"`
+	TemplateId string `mapstructure:"template_id"`
+	SignName   string `mapstructure:"sign_name"`
 }
 
 type WorkWxConf struct {
@@ -59,10 +59,18 @@ type WorkWxConf struct {
 	} `mapstructure:"-"`
 }
 
+type OpenPlatformConf struct {
+	GrpcAddr       string `mapstructure:"grpc_addr"`
+	GrpcCert       string `mapstructure:"grpc_cert"`
+	GrpcServerName string `mapstructure:"grpc_server_name"`
+}
+
+type APMConf struct {
+	ReporterBackground string `mapstructure:"reporter_backend"`
+}
+
 var (
-	SSOConf     = &Conf{}
-	DB          *gorm.DB
-	RedisClient *redis.Client
+	SSOConf = &Conf{}
 )
 
 func InitConf(confFilepath string) error {
@@ -76,6 +84,11 @@ func InitConf(confFilepath string) error {
 		return err
 	}
 
+	validate := validator.New()
+	if err := validate.Struct(SSOConf); err != nil {
+		return err
+	}
+
 	SSOConf.Application.AllowServiceReg = make([]*regexp.Regexp, len(SSOConf.Application.AllowService))
 	for i, service := range SSOConf.Application.AllowService {
 		reg, err := regexp.Compile(service)
@@ -86,48 +99,10 @@ func InitConf(confFilepath string) error {
 	}
 
 	if SSOConf.Application.Mode == "debug" {
-		logrus.WithField("config", SSOConf).Debug("load config")
+		zapx.Info("run mode", zap.String("mode", SSOConf.Application.Mode))
 	}
 
 	SSOConf.WorkWx.RedirectUri = url.PathEscape(SSOConf.WorkWx.RedirectUri)
 
 	return nil
-}
-
-func InitDB(ctx context.Context) (err error) {
-	// connect postgres
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN: SSOConf.Database.PostgresDSN,
-	}))
-	if err != nil {
-		logrus.WithError(err).Error("open gorm error")
-		return err
-	}
-	logrus.Info("conect to postgres success")
-	DB = db
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	RedisClient, err = initRedis(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func initRedis(ctx context.Context) (*redis.Client, error) {
-	// init redis
-	rclient := redis.NewClient(&redis.Options{
-		Addr:     SSOConf.Redis.Addr,
-		Password: SSOConf.Redis.Password,
-		DB:       SSOConf.Redis.DB,
-	})
-	pong, err := rclient.Ping(ctx).Result()
-	if err != nil {
-		logrus.WithError(err).Error("ping redis error")
-		return nil, err
-	}
-	logrus.WithField("result", pong).Info("ping redis success")
-	return rclient, nil
 }
