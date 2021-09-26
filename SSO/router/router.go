d9acd32c04ed14a20a6e9a01542a5996f99d3f58
xylonx <xylonx@dingtalk.com>
2021-09-26 11:19:44 +0800

    :fire::sparkles: delete register routter & add middleware

--- SSO/router/router.go
+++ SSO/router/router.go
@@ -2,19 +2,22 @@ package router
 
 import (
 	"unique/jedi/controller"
+	"unique/jedi/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(r *gin.Engine) {
+	r.Use(middleware.TracingMiddleware())
+	r.Use(middleware.Cors())
+	r.Use(gin.Recovery())
+	r.Use(gin.Logger())
+
 	// CAS related
 	sr := r.Group("/cas")
 	sr.POST("/login", controller.Login)
 	sr.POST("/logout", controller.Logout)
 	sr.GET("/p3/serviceValidate", controller.ValidateTicket)
-
-	// register function
-	r.POST("/register", controller.Register)
 
 	smsrouter := r.Group("/sms")
 	smsrouter.POST("code", controller.SendSmsCode)
